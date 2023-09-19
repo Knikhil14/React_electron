249dee86ccb382f7aa3862e6723665990a7177e1
Ubuntu <ubuntu@ip-172-31-11-241.ap-south-1.compute.internal>
2023-09-19 06:59:20 +0000

    first commit

--- src/electron-wait-react.js
+++ src/electron-wait-react.js
@@ -6,23 +6,35 @@ process.env.ELECTRON_START_URL = `http://localhost:${port}`;
 const client = new net.Socket();
 
 let startedElectron = false;
-const tryConnection = () => client.connect({port: port}, () => {
-        client.end();
-        if(!startedElectron) {
-            console.log('starting electron');
-            startedElectron = true;
-            const exec = require('child_process').exec;
-            const electron = exec('npm run electron');
-            electron.stdout.on("data", function(data) {
-                console.log("stdout: " + data.toString());
-            });
-        }
+const tryConnection = () => {
+  client.connect({ port: port }, () => {
+    client.end();
+    if (!startedElectron) {
+      console.log('Starting electron');
+      startedElectron = true;
+      const exec = require('child_process').exec;
+      exec('npm run electron');
     }
-);
+  });
+};
 
 tryConnection();
 
 client.on('error', (error) => {
-    setTimeout(tryConnection, 1000);
+  setTimeout(tryConnection, 1000);
 });
+
+// Handle process termination signal (SIGTERM)
+process.on('SIGTERM', () => {
+  console.log('Received SIGTERM. Stopping Electron.');
+  process.exit(0);
+});
+
+
+const startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, '/../build/index.html'),
+    protocol: 'file:',
+    slashes: true
+});
+mainWindow.loadURL(startUrl);
 
